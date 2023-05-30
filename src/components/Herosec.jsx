d68ea8724d8b6ea6374ff52eded0d66bd31dce54
BerwalShubham <shubhamberwal0010@gmail.com>
2023-05-30 11:11:43 +0530

    first commit

---
+++ src/components/Herosec.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import heroimg from "../assets/png/Herosecimg.png";
+import ellipse from "../assets/svg/Ellipse.svg";
+
+const Herosec = () => {
+  return (
+    <section className="bg-img position-relative overflow_hidden">
+      <Container className="position-relative">
+        <Row className="flex-column-reverse flex-lg-row">
+          <Col className="position-relative" lg={7}>
+            <img className="ellipse" src={ellipse} alt="elipse" />
+            <div className="text-bg-color mt-4 pb-4 pb-lg-0 d-lg-flex flex-column justify-content-center text-center text-lg-start">
+              <h1 className="ff-nunito fs-xl heading fw-black">
+                Get Bussiness <br /> Solutions with TheFirm.
+              </h1>
+              <p className="ff-lora fs-xsm pt-4">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy.
+              </p>
+              <button className="btn-get-in-touch fw-bold mt-4 mx-auto mx-lg-0">
+                Get in Touch
+              </button>
+            </div>
+          </Col>
+          <Col lg={5}>
+            <img
+              className="heroimg w-100"
+              src={heroimg}
+              alt="Meeting room in office"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Herosec;
---
+++ src/components/TheFirmNav.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+import logo from "../assets/svg/logo.svg";
+
+const TheFirmNav = () => {
+  const [nav, setnav] = useState(true);
+  if (nav) {
+    document.body.style.overflow = "visible";
+  } else {
+    document.body.style.overflow = "hidden";
+  }
+  return (
+    <section className="thefirm-nav py-2">
+      <Container>
+        <div className="d-flex align-items-center justify-content-between">
+          <a href="#">
+            <img className="logo" src={logo} alt="logo" />
+          </a>
+          <div className="d-flex align-items-center gap-5">
+            <ul
+              className={
+                nav
+                  ? "nav-hidden d-flex align-items-center justify-content-lg-end gap-4 py-2 mb-0"
+                  : "nav-open d-xl-none p-0 gap-5"
+              }
+            >
+              <li className="px-lg-1 px-xxl-0">
+                <a
+                  className="ff-nunito fw-bold fs-xsm color-black position-relative"
+                  href="#"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="px-lg-1 px-xxl-0">
+                <a
+                  className="ff-nunito fw-bold fs-xsm color-black position-relative"
+                  href="#"
+                >
+                  Solution
+                </a>
+              </li>
+              <li className="px-lg-1 px-xxl-0">
+                <a
+                  className="ff-nunito fw-bold fs-xsm color-black position-relative"
+                  href="#"
+                >
+                  Cases
+                </a>
+              </li>
+              <li className="px-lg-1 px-xxl-0">
+                <a
+                  className="ff-nunito fw-bold fs-xsm color-black position-relative"
+                  href="#"
+                >
+                  About Us
+                </a>
+              </li>
+              <li className="px-lg-1 px-xxl-0">
+                <a
+                  className="ff-nunito fw-bold fs-xsm color-black position-relative"
+                  href="#"
+                >
+                  Blog
+                </a>
+              </li>
+              <li className="px-lg-1 px-xxl-0">
+                <a
+                  className="ff-nunito fw-bold fs-xsm color-black position-relative"
+                  href="#"
+                >
+                  Contact Us
+                </a>
+              </li>
+              <button className="btn-get-in-touch fw-bold mx-auto mx-lg-0 d-md-none">
+                Get in Touch
+              </button>
+            </ul>
+            <button className="btn-get-in-touch fw-bold mx-auto mx-lg-0 d-none d-md-block">
+              Get in Touch
+            </button>
+            <div className="text-end d-xl-none">
+              <button
+                className={nav === true ? "hamburger" : "hamburger-2"}
+                onClick={() => setnav(!nav)}
+              >
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default TheFirmNav;
